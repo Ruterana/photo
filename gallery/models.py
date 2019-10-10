37f1161c35e723d2ae396707259282234a78416d
Ruterana <ncharlene38@gmail.com>
2019-10-10 12:04:28 +0200

    iadd  model.py and location class

--- gallery/models.py
+++ gallery/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Location(models.Model):
+    location_name= models.CharField(max_length =30,unique=True)
+    def __str__(self):
+        return self.location_name
+    def save_location(self):
+        self.save()
