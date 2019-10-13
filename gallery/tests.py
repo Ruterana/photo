35bceda56f91e6d08693ec4dd8837b777b8dd02b
Ruterana <ncharlene38@gmail.com>
2019-10-13 15:57:15 +0200

    I add link of photos

--- gallery/tests.py
+++ gallery/tests.py
@@ -1,3 +1,19 @@
 from django.test import TestCase
 
 # Create your tests here.
+
+from .models import Location,Category,Image
+
+class LocationTestClass(TestCase):
+
+    # Set up method
+    def setUp(self):
+        self.Location(location_name="nyungwe")
+    # Testing  instance
+    def test_instance(self):
+        self.assertTrue(isinstance(self.nyungwe,Location))
+    # Testing Save Method
+    def test_save_method(self):
+        self.location.save_location()
+        locations = Location.objects.all()
+        self.assertTrue(len(locations) > 0)
