448c1d47018a72dcf2cffdb87cfd6ad6f4bbe94c
Ruterana <ncharlene38@gmail.com>
2019-10-13 19:38:05 +0200

    iadd test.py

--- gallery/tests.py
+++ gallery/tests.py
@@ -1,19 +1,59 @@
+
 from django.test import TestCase
-
-# Create your tests here.
-
 from .models import Location,Category,Image
 
-class LocationTestClass(TestCase):
+# Create your tests here.
+class ImageTestClass(TestCase):
+   # Setup method
+   def setUp(self):
+       self.location = Location(location='nyungwe')
+       self.location.save()
+       self.category = Category(category = 'Animal')
+       self.category.save()
+       self.image = Image(image_name = 'cat.jpg', name='test',description='this is the my favorite animal
 
-    # Set up method
-    def setUp(self):
-        self.Location(location_name="nyungwe")
-    # Testing  instance
-    def test_instance(self):
-        self.assertTrue(isinstance(self.nyungwe,Location))
-    # Testing Save Method
-    def test_save_method(self):
-        self.location.save_location()
-        locations = Location.objects.all()
-        self.assertTrue(len(locations) > 0)
+',location = self.location, image_category = self.category)
+   def tearDown(self):
+       Image.objects.all().delete()
+       Location.objects.all().delete()
+       Category.objects.all().delete()
+   # Testing Instance
+   def test_instance(self):
+       self.assertTrue(isinstance(self.image,Image))
+   # testing the save method
+   def test_save_method(self):
+       self.image = Image(pic_image = 'cat.jpg', name='test',description='this is the my favorite animal',location = self.location, image_category = self.category)
+       self.image.save_image()
+       images = Image.objects.all()
+       self.assertTrue(len(images) >= 1)
+   def test_delete_method(self):
+       self.image = Image(pic_image = 'cat.jpg', name='test',description='this is the my favorite animal',location = self.location, image_category = self.category)
+       self.image.save_image()
+       images = self.image.delete_image()
+       deleted = Image.objects.all()
+       self.assertTrue(len(deleted) <= 0)
+class LocationTestClass(TestCase):
+   # SetUp Class
+   def setUp(self):
+       self.location = Location(location="NYUNGWE")
+   def tearDown(self):
+       Location.objects.all().delete()
+   def test_save_location(self):
+       self.location.save_location()
+       location = Location.objects.all()
+       self.assertTrue(len(location)>= 1)
+class CategoryTestClass(TestCase):
+   # SetUp Class
+   def setUp(self):
+       self.category = Category(category="CAT")
+   def tearDown(self):
+       Category.objects.all().delete()
+   def test_save_category(self):
+       self.category.save_category()
+       category = Category.objects.all()
+       self.assertTrue(len(category)>= 1)
+   def test_delete_category(self):
+       self.category.save_category()
+       categories = self.category.delete_category()
+       category = Category.objects.all()
+       self.assertTrue(len(category) <= 0)
